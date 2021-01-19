defb4f4a16f6beccea31953b05df49ef325e77f9
David Mueses <davidmueses0@gmail.com>
2021-01-19 12:05:46 -0500

    Pan_7

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ClientesController;
-use App\Http\Controllers\PanaderiaController;
-use App\Http\Controllers\ProductosController;
-use App\Http\Controllers\Productos1Controller;
-use App\Http\Controllers\VentasController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,38 +13,12 @@ use App\Http\Controllers\VentasController;
 |
 */
 
-//Ruta /
-Route::get('/',[PanaderiaController::class,'getIndex']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-//Ruta Categoria
-Route::get('categoria',[ProductosController::class,'Categoria'])->name('listado');
-Route::post('categoria',[ProductosController::class,'registrar']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-//Ruta Productos
-Route::get('productos',[Productos1Controller::class,'Productos'])->name('listadoP');
-Route::get('productos/registro',[Productos1Controller::class,'formularioReg']);
-Route::post('productos/registro',[Productos1Controller::class,'registrar'])->name('registraP');
-Route::get('productos/consulta', [Productos1Controller::class, 'form_consulta'])->name('formConsulta');
-Route::post('productos/consulta', [Productos1Controller::class, 'consultar'])->name('consulta');
-
-
-//Ruta Clientes
-Route::get('clientes',[ClientesController::class,'getClientes']);
-
-//Ruta Ventas
-Route::get('ventas',[VentasController::class,'getVentas']);
-
-//Ruta Servicios/Habitaciones
-//Route::get('servicios/habitaciones',[HabitacionesController::class,'showHabitaciones']);
-
-//Ruta Clientes/Visualizar
-//Route::get('clientes/visualizar',[ClientesController::class,'showClientes']);
-
-//Ruta Facturacion
-//Route::get('facturacion/factura',[FacturacionController::class,'getFactura']);
-
-//Ruta Reservas
-//Route::get('reservas',[ReservasController::class,'getReservas']);
-
-//Ruta Contactos
-//Route::get('contactos',[HotelController::class,'showContactos']);
+require __DIR__.'/auth.php';
