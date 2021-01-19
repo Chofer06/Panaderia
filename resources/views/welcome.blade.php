1d11369df6e39289a256527a8fcbda5660096255
David Mueses <davidmueses0@gmail.com>
2021-01-19 12:46:17 -0500

    Pan_8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.master')
 @section('content')
-	<br>
-	<h1 align="center"><strong>Pantalla Principal Panderia</strong></h1>
+	<h1 align="center"><strong>Pantalla Principal Panaderia</strong></h1>
 	<!--img src="{{url('imagenes/1.jpg')}}" height="480" width="1100"-->
 @stop
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\PanaderiaController;
+use App\Http\Controllers\ProductosController;
+use App\Http\Controllers\Productos1Controller;
+use App\Http\Controllers\VentasController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,12 +19,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+//Ruta /
+Route::get('/',[PanaderiaController::class,'getIndex']);
+
+//Ruta Categoria
+Route::get('categoria',[ProductosController::class,'Categoria'])->name('listado');
+Route::post('categoria',[ProductosController::class,'registrar']);
+
+//Ruta Productos
+Route::get('productos',[Productos1Controller::class,'Productos'])->name('listadoP');
+Route::get('productos/registro',[Productos1Controller::class,'formularioReg']);
+Route::post('productos/registro',[Productos1Controller::class,'registrar'])->name('registraP');
+Route::get('productos/consulta', [Productos1Controller::class, 'form_consulta'])->name('formConsulta');
+Route::post('productos/consulta', [Productos1Controller::class, 'consultar'])->name('consulta');
+
+
+//Ruta Clientes
+Route::get('clientes',[ClientesController::class,'getClientes']);
+
+//Ruta Ventas
+Route::get('ventas',[VentasController::class,'getVentas']);
 
 require __DIR__.'/auth.php';
